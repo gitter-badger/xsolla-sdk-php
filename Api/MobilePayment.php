508d6b045efe30d0259b2734b2a6c3a2f5a1bd4a
Vitaliy Zakharov <zakharovvi@gmail.com>
2012-11-08 02:20:20 +0600

    add invoice create method to Mobile payment

--- Api/MobilePayment.php
+++ Api/MobilePayment.php
@@ -4,6 +4,7 @@ namespace Xsolla\Sdk\Api;
 use Xsolla\Sdk\Api\Client\ClientInterface;
 use Xsolla\Sdk\Api\Exception\ErrorCode\MobilePaymentException;
 use Xsolla\Sdk\Api\Exception\ErrorCode\ErrorCodeExceptionInterface;
+use Xsolla\Sdk\Api\Exception\InvalidResponseException;
 
 /**
  * @link http://xsolla.com/docs/mobile-payment-api
@@ -18,7 +19,7 @@ class MobilePayment
     const XSD_PATH_INVOICE = '/mobilepayment/invoice.xsd';
 
     /**
-     * @var string
+     * @var int
      */
     private $projectId;
 
@@ -39,9 +40,10 @@ class MobilePayment
 
     /**
      * @param  ClientInterface           $client
-     * @param  int                       $projectId
-     * @param  string                    $secretKey
-     * @throws \InvalidArgumentException
+     * @param  string $schemaDir         /path/to/Xsolla/Sdk/Resources/schema/api
+     * @param  int                       $projectId Developer's ID in Xsolla system.
+     * @param  string                    $secretKey Developer's secret key.
+     * @throws \InvalidArgumentException If one of arguments has invalid format.
      */
     public function __construct(ClientInterface $client, $schemaDir, $projectId, $secretKey)
     {
@@ -60,24 +62,96 @@ class MobilePayment
         $this->secretKey = $secretKey;
     }
 
-    public function invoice()
+    /**
+     * Method is used for generating an invoice to be paid by the user.
+     * @param  string                    $phone  User's phone number. Must be 10 digits. Example 9123456789.
+     * @param  string                    $v1     User ID unique to developer's platform
+     * @param  string                    $v2     User ID (additional). Required if v3 is present.
+     * @param  string                    $v3     User ID (additional)
+     * @param  float                     $sum    Payment amount in RUB. $sum and $out cannot both be present. Must contain 2 or fewer digits after the decimal point.
+     * @param  float                     $out    Virtual currency amount. $sum and $out cannot both be present.
+     * @param  string                    $email  User's email
+     * @param  string                    $userIp User's IP address
+     * @return string                    Invoice number.
+     * @throws \InvalidArgumentException If one of arguments has invalid format.
+     * @throws InvalidResponseException  If network problem or response has invalid format.
+     * @throws MobilePaymentException    If xsolla response is not succeed.
+     */
+    public function invoice($phone, $v1, $v2 = null, $v3 = null, $sum = null, $out = null, $email = null, $userIp = null)
     {
+        $urlVars = array('command' => 'invoice', 'project' => $this->projectId, 'v1' => $v1);
+        $stringForSignature = 'invoice'.$this->projectId.$v1;
+        if (!is_null($v2)) {
+            $urlVars['v2'] = $v2;
+            if (!is_null($v3)) {
+                $urlVars['v3'] = $v3;
+            }
+        }
+        //sum & out
+        if (!is_null($sum) AND !is_null($out)) {
+            throw new \InvalidArgumentException('Sum and out cannot both be present.');
+        } elseif (!is_null($sum)) {
+            if (!$this->isValidSumFormat($sum, true)) {
+               throw new \InvalidArgumentException("$sum - out has invalid format.");
+            }
+            $urlVars['sum'] = $sum;
+            $stringForSignature .= $sum;
+        } elseif (!is_null($out)) {
+            if (!$this->isValidSumFormat($out, false)) {
+               throw new \InvalidArgumentException("$out - out has invalid format.");
+            }
+            $urlVars['out'] = $out;
+            $stringForSignature .= $out;
+        } else {
+            throw new \InvalidArgumentException('Sum or out must be passed.');
+        }
+        //phone
+        if (!$this->isValidPhoneFormat($phone)) {
+            throw new \InvalidArgumentException("$phone - phone number must contain only digits and have a length of 10 characters");
+        }
+        $urlVars['phone'] = $phone;
+        $stringForSignature .= $phone;
+        //ip
+        if (!is_null($userIp)) {
+            if (!filter_var($userIp, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+                throw new \InvalidArgumentException;
+            }
+            $urlVars['userip'] = $userIp;
+            $stringForSignature .= $userIp;
+        }
+        //email
+        if (!is_null($email)) {
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                throw new \InvalidArgumentException;
+            }
+            $urlVars['email'] = $email;
+            $stringForSignature .= $email;
+        }
+        $urlVars['md5'] = md5($stringForSignature.$this->secretKey);
+        $url = self::URL.http_build_query($urlVars);
+        $xsdFileName = $this->schemaDir.self::XSD_PATH_INVOICE;
+        $xsollaResponse = $this->client->send($url, $xsdFileName);
+        if ('0' !== $xsollaResponse['result']) {
+            throw new MobilePaymentException($xsollaResponse['comment'], $xsollaResponse['result']);
+        }
+        if (!isset($xsollaResponse['invoice'])) {
+            throw new InvalidResponseException('Xsolla response doesn\'t contain invoice number.');
+        }
 
+        return $xsollaResponse['invoice'];
     }
 
     /**
-     * @param  int                         $phone
-     * @param  float                       $out
-     * @return float
+     * Calculating the payment amount in roubles that the usershould pay in order to receive a certain amount of virtual currency.
+     * @param  string                         $phone User's phone number. Must be 10 digits. Example 9123456789.
+     * @param  float                       $out   Virtual currency amount.
+     * @return float                       Payment amount in RUB.
      * @throws \InvalidArgumentException   If phone or sum has invalid format
      * @throws InvalidResponseException    If network problem or response has invalid format.
      * @throws ErrorCodeExceptionInterface If xsolla response is not succeed.
      */
     public function calculateSum($phone, $out)
     {
-        if (!$this->isValidPhoneFormat($phone)) {
-            throw new \InvalidArgumentException("$phone - phone number must contain only digits and have a length of 10 characters");
-        }
         if (!$this->isValidSumFormat($out)) {
             throw new \InvalidArgumentException("$out - out is not a float");
         }
@@ -86,18 +160,16 @@ class MobilePayment
     }
 
     /**
-     * @param  int                         $phone
-     * @param  float                       $sum
-     * @return float
+     * Calculating the virtual currency amount the user receives for their payment in roubles.
+     * @param  string                      $phone User's phone number. Must be 10 digits. Example 9123456789.
+     * @param  float                       $sum   Payment amount in RUB. Must contain 2 or fewer digits after the decimal point.
+     * @return float                       Virtual currency amount.
      * @throws \InvalidArgumentException   If phone or sum has invalid format
      * @throws InvalidResponseException    If network problem or response has invalid format.
      * @throws ErrorCodeExceptionInterface If xsolla response is not succeed.
      */
     public function calculateOut($phone, $sum)
     {
-        if (!$this->isValidPhoneFormat($phone, true)) {
-            throw new \InvalidArgumentException("$phone - phone number must contain only digits and have a length of 10 characters");
-        }
         if (!$this->isValidSumFormat($sum)) {
             throw new \InvalidArgumentException("$sum - sum is not a float");
         }
@@ -107,6 +179,9 @@ class MobilePayment
 
     private function calculate($phone, $number, $numberType)
     {
+        if (!$this->isValidPhoneFormat($phone)) {
+            throw new \InvalidArgumentException("$phone - phone number must contain only digits and have a length of 10 characters");
+        }
         $urlVars = array('command' => 'calculate', 'project' => $this->projectId);
         $stringForSignature = 'calculate'.$this->projectId;
         $urlVars[$numberType] = $number;
--- Tests/Api/MobilePaymentTest.php
+++ Tests/Api/MobilePaymentTest.php
@@ -4,7 +4,7 @@ namespace Xsolla\Sdk\Tests\Api;
 use Xsolla\Sdk\Api\MobilePayment;
 
 /**
- * @author Vitaliy Zakharov <zakharovvi@gmail.com>
+ * @author Vitaliy Zakharov <v.zakharov@xsolla.com>
  */
 class MobilePaymentTest extends \PHPUnit_Framework_TestCase
 {
@@ -55,6 +55,8 @@ class MobilePaymentTest extends \PHPUnit_Framework_TestCase
      */
     public function testCalculateOutFailWhenPhoneHasInvalidFormat()
     {
+        $this->clientMock->expects($this->never())
+            ->method('send');
         $this->mobilePayment->calculateOut('123456789', 1);
     }
 
@@ -63,6 +65,8 @@ class MobilePaymentTest extends \PHPUnit_Framework_TestCase
      */
     public function testCalculateSumFailWhenSumHasInvalidFormat()
     {
+        $this->clientMock->expects($this->never())
+            ->method('send');
         $this->mobilePayment->calculateSum(self::PHONE, 0);
     }
 
@@ -95,6 +99,7 @@ class MobilePaymentTest extends \PHPUnit_Framework_TestCase
             'comment' => 'ok',
         );
         $expectedXsdFileName = self::SCHEMA_DIR.MobilePayment::XSD_PATH_CALCULATE;
+        //$md5 = md5('calculate4783109630123817key');
         $expectedUrl = 'https://api.xsolla.com/mobile/payment/index.php?command=calculate&project=4783&sum=10&phone=9630123817&md5=15ab2801202b594dc0706176616771d2';
         $this->clientMock->expects($this->once())
                 ->method('send')
@@ -103,6 +108,108 @@ class MobilePaymentTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(
             1.5,
             $this->mobilePayment->calculateOut(self::PHONE, 10)
+        );
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvoiceFailWhenPhoneIsNotValid()
+    {
+        $this->clientMock->expects($this->never())
+            ->method('send');
+        $this->mobilePayment->invoice(123456789, 'demo', null, null, 11);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvoiceFailWhenEmailIsNotValid()
+    {
+        $this->clientMock->expects($this->never())
+            ->method('send');
+        $this->mobilePayment->invoice(self::PHONE, 'demo', null, null, 11, null, 'invalidEmail');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvoiceFailWhenUserIpIsNotValid()
+    {
+        $this->clientMock->expects($this->never())
+            ->method('send');
+        $this->mobilePayment->invoice(self::PHONE, 'demo', null, null, 11, null, 'example@example.com', '127.0.0.00');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvoiceFailWhenSumAndOutPassed()
+    {
+        $this->clientMock->expects($this->never())
+            ->method('send');
+        $this->mobilePayment->invoice(self::PHONE, 'demo', null, null, 11, 11);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvoiceFailWhenSumAndOutNotPassed()
+    {
+        $this->clientMock->expects($this->never())
+            ->method('send');
+        $this->mobilePayment->invoice(self::PHONE, 'demo');
+    }
+
+    /**
+     * @expectedException \Xsolla\Sdk\Api\Exception\ErrorCode\MobilePaymentException
+     * @expectedExceptionCode 3
+     * @expectedExceptionMessage error comment 3
+     */
+    public function testInvoiceFailWhenXsollaResponseNotSucceed()
+    {
+        $expectedXsdFileName = self::SCHEMA_DIR.MobilePayment::XSD_PATH_INVOICE;
+        $this->clientMock->expects($this->once())
+            ->method('send')
+            ->with(
+                //$md5 = md5('invoice4783demo12.459120000000key');
+                'https://api.xsolla.com/mobile/payment/index.php?command=invoice&project=4783&v1=demo&sum=12.45&phone=9120000000&md5=d1e7b81eeebf2c6d48f7481ecea0562c',
+               $expectedXsdFileName
+            )
+            ->will($this->returnValue(array('result' => '3', 'comment' => 'error comment 3')));
+        $this->mobilePayment->invoice(9120000000, 'demo', null, null, 12.45);
+    }
+
+    /**
+     * @expectedException \Xsolla\Sdk\Api\Exception\InvalidResponseException
+     * @expectedExceptionMessage Xsolla response doesn't contain invoice number.
+     */
+    public function testInvoiceFailWhenRepsonseCodeIsSucceedButInvoiceUndefined()
+    {
+        $this->clientMock->expects($this->once())
+            ->method('send')
+            ->will($this->returnValue(array('result' => '0', 'comment' => 'Not ok')));
+        $this->mobilePayment->invoice(9120000000, 'demo', null, null, 12.45);
+    }
+
+    public function testInvoice()
+    {
+        $expectedXsdFileName = self::SCHEMA_DIR.MobilePayment::XSD_PATH_INVOICE;
+        $this->clientMock->expects($this->once())
+            ->method('send')
+            ->with(
+                //$md5 = md5('invoice4783demo543.89999120000000192.33.19.70mail@example.comkey');
+                'https://api.xsolla.com/mobile/payment/index.php?command=invoice&project=4783&v1=demo&out=543.8999&phone=9120000000&userip=192.33.19.70&email=mail%40example.com&md5=3ee70d833166f544674d67767969a84e',
+               $expectedXsdFileName
+            )
+            ->will($this->returnValue(array(
+                'result'  => '0',
+                'comment' => 'OK',
+                'invoice' => '141235145',
+            )));
+        $this->assertEquals(
+            '141235145',
+            $this->mobilePayment->invoice(9120000000, 'demo', null, null, null, 543.8999, 'mail@example.com', '192.33.19.70')
         );
     }
 }
